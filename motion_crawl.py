efffbbc81ce1d44eb5a4422fff6c2a135f6d4778
Marcus Schwartz <marcus@schwartz.org>
2018-07-14 23:58:29 -0700

    fix crash when going into unfinished crawl mode

--- motion_crawl.py
+++ motion_crawl.py
@@ -5,3 +5,9 @@ from motion import MotionController
 
 class CrawlMC(MotionController):
     _name = "CRAWL"
+
+    def submode(self):
+    	return "NONE"
+
+    def motor_speeds(self):
+        return 0.0, 0.0
