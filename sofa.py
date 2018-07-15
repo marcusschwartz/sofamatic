29f31246625ab560fd5b5737cef50a687a5d4570
Marcus Schwartz <marcus@schwartz.org>
2018-07-15 15:09:17 -0700

    only call status() on once per loop.  maintain trailing average of packet loss rate, jitter, etc.

--- sofa.py
+++ sofa.py
@@ -10,6 +10,7 @@
   motor speeds via the controller, and then passes the motor speeds to the
   roboteq.
 """
+import collections
 import json
 import socket
 import time
@@ -29,33 +30,37 @@ class Sofa(object):
         self._controller = motion_complex.ComplexMotionController()
         self._udp_status_delay = 0
 
-    def update_status_file(self, joystick, status):
+    def update_status_file(self, joystick_status, roboteq_status, controller_status, status):
         if not self._status_path:
             return
         details = util.merge_status_details({
-            "j": joystick.status().details,
-            "m": self._roboteq.status().details,
-            "c": self._controller.status().details,
+            "j": joystick_status.details,
+            "m": roboteq_status.details,
+            "c": controller_status.details,
             "l": status,
         })
         details["ts"] = int(time.time())
         json.dump(details, open(self._status_path, "w"))
 
-    def status_string(self, joystick, status_string):
-        return " ".join([self._controller.status().string,
-                         joystick.status().string,
-                         self._roboteq.status().string,
+    def status_string(self, joystick_status, roboteq_status, controller_status, status_string):
+        return " ".join([controller_status.string,
+                         joystick_status.string,
+                         roboteq_status.string,
                          status_string])
 
-    def send_status_packet(self, joystick, status):
+    def send_status_packet(self, addr, joystick_status, roboteq_status, controller_status, status):
         self._udp_status_delay -= 1
-        if not joystick.addr() or self._udp_status_delay > 0:
+        if not addr or self._udp_status_delay > 0:
             return
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        clock = time.strftime("%I:%M:%S%p").lstrip("0")
-        voltage = " %4.1fv" % self._roboteq.status().details["volts_12"]
-        packet = " ".join((clock, voltage))
-        sock.sendto(packet, joystick.addr())
+        clock = time.strftime("%I:%M:%S").lstrip("0")
+        voltage = "%4.1fv" % roboteq_status.details["volts_12"]
+        brake = roboteq_status.details["brake"]
+        pl = "%3d%%" % (100 - status["packet_loss"])
+        packet = " ".join((clock, voltage, pl))
+        if brake:
+            packet += "~**PARKING BRAKE**"
+        sock.sendto(packet, addr)
         self._udp_status_delay = 10
 
     def run(self):
@@ -63,6 +68,7 @@ class Sofa(object):
         timeout = 0
         INTERVAL = 0.1  # seconds
         GRACE = 0.2  # percent of INTERVAL, 0.0-1.0
+        packet_history = collections.deque()
         while True:
             now = time.time()
             cycle_time = now - last_rcv
@@ -77,14 +83,41 @@ class Sofa(object):
                 "interval": packet_interval,
             }
 
-            status_string = "%3d%% %4dms" % (100 * cycle_time / INTERVAL,
-                                             packet_interval)
+            packet_history.append(tuple((int(100 * cycle_time / INTERVAL),
+                                            packet_interval, joystick)))
+            if len(packet_history) > 10:
+                packet_history.popleft()
+
+            loop_util_total = 0
+            jitter_total = 0
+            interval_total = 0
+            missing_total = 0
+            for [old_loop_util, old_interval, old_joystick] in packet_history:
+                loop_util_total += old_loop_util
+                interval_total += old_interval
+                jitter_total += abs(100 - old_interval)
+                if not old_joystick.valid():
+                    missing_total += 1
+
+            loop_util_avg = loop_util_total / len(packet_history)
+            jitter_avg = jitter_total / len(packet_history)
+            interval_avg = interval_total / len(packet_history)
+            packet_loss_pct = int(100 * missing_total / len(packet_history))
+
+            stats["packet_loss"] = packet_loss_pct
+
+            status_string = "%3d%% %4dms %4dms %2dms %3d%%" % (100 * cycle_time / INTERVAL,
+                                             packet_interval, interval_avg, jitter_avg, packet_loss_pct)
 
             self._controller.update_joystick(joystick)
 
             left_motor, right_motor = self._controller.motor_speeds()
             self._roboteq.set_speed(left_motor, right_motor)
 
-            self.update_status_file(joystick, stats)
-            self.send_status_packet(joystick, stats)
-            print self.status_string(joystick, status_string)
+            joystick_status = joystick.status()
+            roboteq_status = self._roboteq.status()
+            controller_status = self._controller.status()
+
+            self.update_status_file(joystick_status, roboteq_status, controller_status, stats)
+            self.send_status_packet(joystick.addr(), joystick_status, roboteq_status, controller_status, stats)
+            print self.status_string(joystick_status, roboteq_status, controller_status, status_string)
