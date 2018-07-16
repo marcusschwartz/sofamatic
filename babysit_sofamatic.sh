683d1afabb19d43481a1cae5b13e2e4c12cfd936
Marcus Schwartz <marcus@schwartz.org>
2018-07-15 22:15:44 -0700

    move sofa-side components into dedicated subdir

--- babysit_sofamatic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-while /bin/true; do
-	/root/sofa/sofamatic.py >/dev/null 2>&1
-done
---
+++ sofa/babysit_sofamatic.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+while /bin/true; do
+	/root/sofa/sofa/sofamatic.py >/dev/null 2>&1
+done
