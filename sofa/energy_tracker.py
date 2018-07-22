65922a0b99a41f0b21363f45e297e47787e24053
Marcus Schwartz <marcus@schwartz.org>
2018-07-22 12:22:24 -0700

    fix energy tracking

--- sofa/energy_tracker.py
+++ sofa/energy_tracker.py
@@ -6,13 +6,13 @@ import status
 
 class EnergyTrackerStatus(status.Status):
     _attrs = ['volts', 'amps_l', 'amps_r', 'watt_hours', 'regen_watt_hours']
-    _dashboard_fmt = ['{volts:4.2f}v', '{amps_l:3.1f}al', '{amps_r:3.1f}ar',
-                      '{0.watts:4.1f}w', '{watt_hours:5.2f}wh',
+    _dashboard_fmt = ['{volts:4.2f}v', '{amps_l:4.1f}al', '{amps_r:4.1f}ar',
+                      '{0.watts:4.0f}w', '{watt_hours:5.2f}wh',
                       '{regen_watt_hours:-6.3f}wh']
 
     @property
     def watts(self):
-        return self[1] * (self[2] + self[3])
+        return self[0] * (self[1] + self[2])
 
 
 class EnergyTracker(object):
@@ -25,14 +25,16 @@ class EnergyTracker(object):
 
     def update(self, volts, amps_l, amps_r):
         now = time.time()
+        duration = now - self._last_update_ts
+        self._last_update_ts = now
 
         self._volts = volts
         self._amps_l = amps_l
-
         self._amps_r = amps_r
+
         watts = volts * (amps_l + amps_r)
-        duration = self._last_update_ts - now
         current_watt_hours = watts * (duration / 3600)
+	#print "duration %s volts %s amps %s %s watts %s wh %s" % (duration, self._volts, self._amps_l, self._amps_r, watts, current_watt_hours)
         if current_watt_hours > 0:
             self._watt_hours += current_watt_hours
         else:
